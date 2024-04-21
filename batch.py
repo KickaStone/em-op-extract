479728897818739fa47abc9fe7605ed0976a4db6
Juncheng Ji <callmejjc@gmail.com>
2024-04-21 14:06:39 +0800

    Update batch.py and models.py for train CNN_2d

--- batch.py
+++ batch.py
@@ -5,6 +5,7 @@ import numpy as np
 from load import get_trace_set, load_meta
 from preprocess import pad_to_length
 from common import *
+import sklearn.preprocessing
 
 filter_method = 'abs_nofilt'
 datasets_root = './datasets/'
@@ -77,4 +78,119 @@ def get_validation_batch(dataset_name, with_bounds=False):
     for i, dataset_file in enumerate(dataset_files):
         if '_traces.npy' in dataset_file:
             for batch in get_batch(dataset_path, dataset_file, batch_c, batch_size=64):
+                return batch
+
+# 
+window_size = 131072 + 256  # 131'328
+
+def normalize(data):
+    # normalize the data to [-1, 1]
+    temp = np.float32(data) - np.min(data)
+    out = (temp / np.max(temp) - 0.5) * 2
+    return out
+
+
+def get_normalized_data_2d(trace):
+    return sklearn.preprocessing.normalize(trace)
+
+def transform_meta_1d_to_2d(meta, overlap=0.5):
+    if "left_bound" in meta:
+        meta["left_bound"] = (meta["left_bound"] / (512 * (1.0 - overlap)))
+        meta["right_bound"] = (meta["right_bound"] / (512 * (1.0 - overlap)))
+    return meta
+
+
+def get_normalized_data(data):
+    data_ = normalize(data)
+    return data_[0:-1], data_[1:]  # 切片到最后一个【0，-1）步长为1
+
+
+def label_to_title(label):
+    label_class = np.argmax(label[0:num_classes])
+    return int_to_op[label_class]
+
+
+def get_batch(dataset_path, dataset_file, batch_c, batch_size=1, wavenet=False, augment=False):
+    trace_name = dataset_file.rpartition('_traces.npy')[0]
+    meta_name = trace_name + '_meta.p'
+    trace_path = os.path.join(dataset_path, dataset_file)
+    meta_path = os.path.join(dataset_path, meta_name)  # 创建两个路径
+
+    # get traces
+    trace_set = get_trace_set(trace_path, 'cw')
+
+    meta_trace_set = load_meta(meta_path)
+
+    for j, trace in enumerate(trace_set.traces):
+        if "bad" in meta_trace_set[j]:  # Skip traces explicitly labeled as "bad"
+            continue
+        filtered_trace = pad_to_length(trace.signal, input_size + 1)  # Padding
+        filtered_trace = filter_trace(filtered_trace, filter_method)  #
+
+        filtered_trace = get_normalized_data(filtered_trace)
+
+        wavenet_input, wavenet_target = get_normalized_data(filtered_trace)
+
+        label = get_onehot(meta_trace_set[j]["op"])
+        # print(f"input={wavenet_input}, target={wavenet_target}, label={label}")
+        batch_c.append((wavenet_input, wavenet_target, label))
+
+        if len(batch_c) == batch_size:
+            batch = np.array(batch_c, dtype=list)  # must declear the dtype as list, or it will raise valueError
+            np.random.shuffle(batch)  # 打乱list排序
+            yield batch
+            batch_c.clear()
+
+
+def get_validation_batch(dataset_name, with_bounds=False):
+    dataset_path = os.path.join(datasets_root, dataset_name)
+    dataset_files = list(os.listdir(dataset_path))
+
+    batch_c = []
+    for i, dataset_file in enumerate(dataset_files):
+        if '_traces.npy' in dataset_file:
+            for batch in get_batch(dataset_path, dataset_file, batch_c, batch_size=64):
+                return batch
+
+
+def get_batch_2d(dataset_path, dataset_file, batch_c, batch_size=1, wavenet=False, augment=False):
+    trace_name = dataset_file.rpartition('_traces.npy')[0]
+    meta_name = trace_name + '_meta.p'
+    trace_path = os.path.join(dataset_path, dataset_file)
+    meta_path = os.path.join(dataset_path, meta_name)  # 创建两个路径
+
+    # get traces
+    trace_set = get_trace_set(trace_path, 'cw')
+    # get tracemetadata
+    meta_trace_set = load_meta(meta_path)
+
+    for j, trace in enumerate(trace_set.traces):
+        if "bad" in meta_trace_set[j]:  # Skip traces explicitly labeled as "bad"
+            continue
+        filtered_trace = pad_to_length(trace.signal, window_size)  # Padding
+        filtered_trace = get_stft(filtered_trace, overlap_rate=0.5, show_plot=False)  #
+        meta_trace_set[j] = transform_meta_1d_to_2d(meta_trace_set[j], overlap=0.5)
+
+        filtered_trace = get_normalized_data_2d(filtered_trace)
+        nn_input = get_normalized_data_2d(filtered_trace)
+
+        label = get_onehot(meta_trace_set[j]["op"])
+        # print(f"input={wavenet_input}, target={wavenet_target}, label={label}")
+        batch_c.append((nn_input, label))
+
+        if len(batch_c) == batch_size:
+            batch = np.array(batch_c, dtype=list)  # must declear the dtype as list, or it will raise valueError
+            np.random.shuffle(batch)  # 打乱list排序
+            yield batch
+            batch_c.clear()
+
+
+def get_validation_batch_2d(dataset_name, with_bounds=False):
+    dataset_path = os.path.join(datasets_root, dataset_name)
+    dataset_files = list(os.listdir(dataset_path))
+
+    batch_c = []
+    for i, dataset_file in enumerate(dataset_files):
+        if '_traces.npy' in dataset_file:
+            for batch in get_batch_2d(dataset_path, dataset_file, batch_c, batch_size=64):
                 return batch
--- models.py
+++ models.py
@@ -44,3 +44,37 @@ class CNN_1d(nn.Module):
         x = self.fc3(x)
         return x
     
+class CNN_2d(nn.Module):
+    def __init__(self):
+        super(CNN_2d, self).__init__()
+        # input : [1, 512, 512] 
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding='same') # output: [16, 512, 512]
+        self.avgpool1 = nn.AvgPool2d(kernel_size=2, stride=2) # output: [16, 256, 256]
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding='same') # output: [32, 256, 256]
+        self.avgpool2 = nn.AvgPool2d(kernel_size=2, stride=2) # output: [32, 128, 128]
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding='same') # output: [64, 128, 128]
+        self.avgpool3 = nn.AvgPool2d(kernel_size=2, stride=2) # output: [64, 64, 64]
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding='same') # output: [128, 64, 64]
+        self.avgpool4 = nn.AvgPool2d(kernel_size=2, stride=2) # output: [128, 32, 32]
+        self.conv5 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding='same') # output: [128, 32, 32]
+        self.avgpool5 = nn.AvgPool2d(kernel_size=2, stride=2) # output: [128, 16, 16]
+        self.linear1 = nn.Linear(128*16*16, 2048)
+        self.linear2 = nn.Linear(2048, 2048)
+        self.linear3 = nn.Linear(2048, 9)
+
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.avgpool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.avgpool2(x)
+        x = F.relu(self.conv3(x))
+        x = self.avgpool3(x)
+        x = F.relu(self.conv4(x))
+        x = self.avgpool4(x)
+        x = F.relu(self.conv5(x))
+        x = self.avgpool5(x)
+        x = x.view(-1, 128*16*16)
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        return self.linear3(x)
